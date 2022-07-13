b1803b8a7d3e61e0472696bb4c3381946d22b6cd
Mareaucanard <matthieu.fraiz@epitech.eu>
2022-07-13 17:00:22 +0200

    Add tools

---
+++ src/Tools/Tools.ts
@@ -0,0 +1,26 @@
+import { createAsyncAction } from "typesafe-actions";
+
+type ErrorAction = { error: Error | string };
+
+const Tools = {
+  createAsyncAction: <
+    TRequest = unknown,
+    TSuccess = unknown,
+    TFailure = ErrorAction
+  >(
+    actionName: string
+  ) => {
+    return {
+      ...createAsyncAction(
+        `${actionName}_REQUEST`,
+        `${actionName}_SUCCESS`,
+        `${actionName}_FAILURE`
+      )<TRequest, TSuccess, TFailure>(),
+      REQUEST: `${actionName}_REQUEST`,
+      SUCCESS: `${actionName}_SUCCESS`,
+      FAILURE: `${actionName}_FAILURE`,
+    };
+  },
+};
+
+export default Tools;

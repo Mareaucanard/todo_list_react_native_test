06805011b7bd038b359002cbc03d79aa2699b068
Mareaucanard <matthieu.fraiz@epitech.eu>
2022-07-15 21:56:21 +0200

    Add Wrappers

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
-import main from "./src/Main";
+import Main from "./src/Main";
+import { Provider } from "react-redux";
+import configureStore from "./src/Store/ConfigureStore";
 
-export default function App() {
-  return main()
+export default () => {
+  const store = configureStore();
+
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  )
 }

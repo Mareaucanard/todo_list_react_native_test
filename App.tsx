68367818ba60b15ff24e4427d6e580f4c05193c8
Mareaucanard <matthieu.fraiz@epitech.eu>
2022-07-18 11:27:40 +0200

    Remove some semi colons

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import Main from "./src/Main";
-import { Provider } from "react-redux";
-import configureStore from "./src/Store/ConfigureStore";
+import Main from "./src/Main"
+import { Provider } from "react-redux"
+import configureStore from "./src/Store/ConfigureStore"
 
 export default () => {
-  const store = configureStore();
+  const store = configureStore()
 
   return (
     <Provider store={store}>
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,45 +1,33 @@
-import React from "react";
-import { Button, Text, TextInput, View } from "react-native";
-import { useSelector } from "react-redux";
-import { AppState } from "./Redux/RootReducer";
-import { useState } from "react";
-import RegisterForm from "./Container/LoginForm";
+import React from "react"
+import { Text, View } from "react-native"
+import { useSelector } from "react-redux"
+import { AppState } from "./Redux/RootReducer"
+import RegisterForm from "./Container/RegisterForm"
 
 const mainState = ({ auth }: AppState) => ({
   token: auth.token,
   error: auth.error,
   msg: auth.msg,
   isLoading: auth.isLoading,
-});
+})
 
 function Handlelogin(dispatch: any): void {
-  dispatch();
+  dispatch()
 }
 
 export default function Main(): JSX.Element {
-  const [form, setform] = useState({
-    email: "",
-    password: "",
-    firstname: "",
-    name: "",
-  });
-  const state = useSelector(mainState);
+  const state = useSelector(mainState)
 
-  const handleChange = React.useCallback((form: any) => {
-    setform(form);
-  }, []);
-
-  console.log(form);
   if (state.isLoading === true) {
-    return <Text>Loading</Text>;
+    return <Text>Loading</Text>
   }
   if (state.token != null && state.token != undefined) {
-    return <Text>Your token is:{state.token}</Text>;
+    return <Text>Your token is:{state.token}</Text>
   }
   return (
     <View>
-      <RegisterForm state={form} setForm={setform}/>
+      <RegisterForm/>
       <Text>Not logged in</Text>
     </View>
-  );
+  )
 }
